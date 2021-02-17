f096e1e74a7bad39c178faba774ebf5de1928ae5
Venugopal K R <50396414+Venugopal296@users.noreply.github.com>
2021-02-18 02:35:03 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux'
+
+import dataReducer from "./Store/Reducer";
+import Main from './Screens/Main/Main';
+
 import './App.css';
+
+const store = createStore(dataReducer, applyMiddleware(thunk));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Main />
+      </div>
+    </Provider>
   );
 }
 
